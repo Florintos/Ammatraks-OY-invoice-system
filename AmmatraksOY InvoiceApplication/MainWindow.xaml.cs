288b143e3fdb8f267bd8d9b5333e898801fcd94c
Florintos <Florintsadiku1998@gmail.com>
2024-04-15 16:44:23 +0200

    Initial commit

---
+++ AmmatraksOY InvoiceApplication/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AmmatraksOY_InvoiceApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Password;
+
+            if (username == "Admin" && password == "1234")
+            {
+                MainMenu mainMenuWindow = new MainMenu();
+                mainMenuWindow.Show();
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password. Please try again.", "Login Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
